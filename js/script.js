31f3b1f444b346f9442a245b180cb3c4f3d0d590
JJangYunji <80668447+JJangYunji@users.noreply.github.com>
2022-01-05 20:37:22 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ menu.onclick = () => {
   menu.classList.toggle("fa-times");
   navbar.classList.toggle("active");
 };
+
+const notice1 = document.querySelector(".notice-heading1");
+const notice1_content = document.querySelector(".notice-content1");
+const notice2 = document.querySelector(".notice-heading2");
+const notice2_content = document.querySelector(".notice-content2");
+
+notice1.onclick = () => {
+  notice1_content.classList.toggle("active");
+};
+
+notice2.onclick = () => {
+  notice2_content.classList.toggle("active");
+};
